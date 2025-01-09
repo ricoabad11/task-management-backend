8dd924251d92cea47d679aa42d21c32a84409975
ricoabad11 <ricoabad11@gmail.com>
2025-01-09 16:56:12 +0800

    Update server.js

--- server.js
+++ server.js
@@ -5,13 +5,10 @@ const taskRoutes = require('./routes/taskRoutes');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Middleware
 app.use(express.json());
 
-// Routes
 app.use('/tasks', taskRoutes);
 
-// Start the server
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
